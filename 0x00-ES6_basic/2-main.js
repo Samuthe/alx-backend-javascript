c495f3571c0c8fb506f05f86b2b44fde628d3537
Samuthe <ekatisamuel@gmail.com>
2023-01-25 09:43:08 +0300

    arrow

--- 0x00-ES6_basic/2-main.js
+++ 0x00-ES6_basic/2-main.js
@@ -1,6 +1,5 @@
-import getNeighborhoodsList from './2-arrow';
+// import getNeighborhoodsList from './2-arrow';
 
-// eslint-disable-next-line new-cap
-const neighborhoodsList = new getNeighborhoodsList();
-const res = neighborhoodsList.addNeighborhood('Noe Valley');
-console.log(res);
+// const neighborhoodsList = new getNeighborhoodsList();
+// const res = neighborhoodsList.addNeighborhood('Noe Valley');
+// console.log(res);
