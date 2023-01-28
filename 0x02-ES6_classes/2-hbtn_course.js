d2230ba177c53a9557d29dc7a5716cc47dee4fe5
Samuthe <ekatisamuel@gmail.com>
2023-01-28 19:37:44 +0300

    task4

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,7 +1,43 @@
 export default class HolbertonCourse{
     constructor(name, length, students){
-        this._name = name
-        this._length = length
-        this._students = students
+        this.name = name
+        this.length = length
+        this.students = students
     }
+
+    get name(){
+        return this._name
+    }
+    set name(value){
+        if(typeof(value !== 'string')){
+        throw new typeError('Name must be a string');
+    }
+        return this._name = value
+}
+
+    get length(){
+        return this.length
+    }
+    set length(value){
+        if(typeof(value !== 'number')){
+            throw new typeError('Length must be a number')
+        }
+        return this._length = value
+
+    }
+
+    get students(){
+        return this._students
+    }
+    set students(value){
+        if(!(value instanceof Array)){
+        throw new typeError('Students must be an array of strings')
+        }
+        if(!value.every((student) => typeof(student) === 'string')){
+            throw new typeError('Students must be an array of strings')
+
+        }
+        return this.students
+    }
+        
 }
