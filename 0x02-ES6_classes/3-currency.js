d3c0c977f8e4adc1a92f7748fd303def08321a89
Samuthe <ekatisamuel@gmail.com>
2023-01-28 20:11:40 +0300

    task5

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,28 +1,30 @@
-/**
- * implement a class with get and set functions
- */
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
 
-export default class Currency{
-    constructor(code, name){
-        this.code = code
-        this.name = name
-    }
+  get code() {
+    return this._code;
+  }
 
-    get code(){
-        return this._code
-    }
-    set code(value){
-        return this._code = value
-    }
+  set code(value) {
+    this._code = value;
+  }
 
-    get name(){
-        return this._name
-    }
-    set name(value){
-        return this._name = value
-    }
+  get name() {
+    return this._name;
+  }
 
-    displayFullCurrenct() {
-        return `${this._code} (${this._name})`                
-    }
+  set name(value) {
+    this._name = value;
+  }
+
+  /**
+   * Creates the full string representation of this Currency.
+   * @returns {String}
+   */
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
 }
