e2df521e8534a2c9bbb0193e0f02ac44b0a415fa
Samuthe <ekatisamuel@gmail.com>
2023-01-28 19:43:10 +0300

    task4

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,10 +1,5 @@
-/**
- * Represents a Holberton Course.
- */
 export default class HolbertonCourse {
   /**
-   * Creates a new @see {@link HolbertonCourse}.
-   *
    * @param {String} name - The name of the course.
    * @param {Number} length - How long the course is (in months).
    * @param {String[]} students - The names of students in the course.
@@ -21,10 +16,6 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
-
-  /**
-   * Sets the name of this course.
-   */
   set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
@@ -38,10 +29,6 @@ export default class HolbertonCourse {
   get length() {
     return this._length;
   }
-
-  /**
-   * Sets the length of this course (in months).
-   */
   set length(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
@@ -55,10 +42,6 @@ export default class HolbertonCourse {
   get students() {
     return this._students;
   }
-
-  /**
-   * Sets the names of students in this course.
-   */
   set students(value) {
     if (!(value instanceof Array)) {
       throw new TypeError('Students must be an array of strings');
