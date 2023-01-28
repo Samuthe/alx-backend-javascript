c92840a001c1ff26ac9ad582f0b054f70369caca
Samuthe <ekatisamuel@gmail.com>
2023-01-28 19:59:13 +0300

    task4

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,24 @@
+class Currency{
+    constructor(code, name){
+        this.code = code
+        this.name = name
+    }
+
+    get code(){
+        return this._code
+    }
+    set code(value){
+        return this._code = value
+    }
+
+    get name(){
+        return this._name
+    }
+    set name(value){
+        return this._name = value
+    }
+
+    displayFullCurrenct() {
+        return `${this._code} ${this._name}`                
+    }
+}
