8c53049385c97e759071a63b1b3ec02bb7dde79d
Samuthe <ekatisamuel@gmail.com>
2023-01-28 18:41:15 +0300

    task3

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,10 +1,7 @@
-class HolbertonCourse{
+export default class HolbertonCourse{
     constructor(name, length, students){
         this._name = name
         this._length = length
         this._students = students
     }
 }
-
-const student1 = new HolbertonCourse('fullstack', 1, ['Samue', 'Marvin'])
-console.log(student1.name);
