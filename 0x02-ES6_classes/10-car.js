5c58f06979a9d111b2fdb91472007583560c4ecf
Samuthe <ekatisamuel@gmail.com>
2023-01-30 11:39:40 +0300

    cars

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,10 +1,11 @@
-export default class Car{
-    constructor(brand, motor, color){
-        this.brand = brand
-        this.motor = motor
-        this.color = color
-    }
-    cloneCar() {
-        return new this.constructor(this._brand, this._motor, this._color);
-    }
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
 }
