1c2a47deffbb0d880caee1b4097183d818aa9ffb
Samuthe <ekatisamuel@gmail.com>
2023-01-30 09:45:35 +0300

    sky high building

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,36 +1,20 @@
-/**
- * import class Building
- */
+import Building from './5-building';
 
-import Building from "./5-building";
-/**
- * create a new class that extends from Building
- */
-export default class SkyHighBuilding extends Building{
-    constructor(sqft, floors){
-        //sqft inherits from super class
-        super(sqft)
-        this.floors = floors
-    }
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
 
-    /**
-     * create get and set methods of attributes
-     */
+  get floors() {
+    return this._floors;
+  }
 
-    get floors(){
-        return this._floors
-    }
-    set floors(value){
-        if(typeof(floors) !== 'number'){
-            return this._floors
-        }
-        return this._floors = value
-    }
-    /**
-     * 
-     * @returns override evacuation method
-     */
-    evacuationWarningMessage(){
-        return `Evacuate slowly the ${this.floors} floors`
-    }
+  set floors(value) {
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
 }
