e51fa04d7db7dada4d0494b07daef181e5fddbb2
Samuthe <ekatisamuel@gmail.com>
2023-01-30 11:52:54 +0300

    car instance

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,11 @@
+import Car from "./10-car";
+
+export default class EVCar extends Car{
+    super(brand, motor, color){
+        this.range = this.range
+    }
+
+    cloneCar(){
+        return new Car
+    }
+}
