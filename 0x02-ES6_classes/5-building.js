a8ee3bef8bde553c445ab4d2d7d0e17f28ef7bb5
Samuthe <ekatisamuel@gmail.com>
2023-01-30 08:55:32 +0300

    class Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,31 +1,31 @@
 /**
- * Implementing an abstruct class
+ * implement a class Building
  */
 
-class Building{
-    constructor(sqft){
-        if(this.constructor !== Building){
-            if(typeof this.evacuationWarningMessage !== 'function'){
-                throw new TypeError('Class extending Building must override evacuationWarningMessage')
-            }
-        }
-        this.sqft = sqft
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
+    this.sqft = sqft;
+  }
 
-    /**
-     * 
-     * implement get and set methods
-     */
+  /**
+   * @param {Number} sqft
+   */
+  
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft nust be a number');
+    }
+    this._sqft = sqft;
+  }
 
-    get() {
-        return this.sqft
-    }
-    set(sqft){
-        if(typeof sqft !== 'number'){
-            throw new TypeError('sqft must be a number');
-        }
-        return this._sqft
-    }
+  get sqft() {
+    return this._sqft;
+  }
 }
-
-export default Building
