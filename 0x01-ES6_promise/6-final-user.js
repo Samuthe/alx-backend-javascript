31f161a239d369e5a13ac5324b14ec57ad0cfade
Samuthe <ekatisamuel@gmail.com>
2023-01-31 13:10:57 +0300

    6-final user

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,17 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const myArr = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    myArr.push({ status: 'fulfilled', value: user });
+    await uploadPhoto(fileName);
+  } catch (err) {
+    myArr.push({
+      status: 'rejected',
+      value: `Error: ${fileName} cannot be processed`,
+    });
+  }
+  return myArr;
+}
