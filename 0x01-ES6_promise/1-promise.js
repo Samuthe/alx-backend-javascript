7ad63da15e4d4c637ee1a58280dfbcad9f0ae326
Samuthe <ekatisamuel@gmail.com>
2023-01-31 09:51:49 +0300

    1-promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,8 +1,4 @@
-/**
- * @param {true} success 
- * @returns {Error} reject
- */
- export default function getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) resolve({ status: 200, body: 'Success' });
     else reject(Error('The fake API is not working currently'));
