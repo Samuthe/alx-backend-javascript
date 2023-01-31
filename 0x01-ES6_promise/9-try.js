337eeb542d00343f61f6d15c4f83ba9511336701
Samuthe <ekatisamuel@gmail.com>
2023-01-31 14:28:08 +0300

    9-try

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -4,6 +4,8 @@ export default function guardrail(mathFunction) {
     const res = mathFunction();
     queue.push(res);
   } catch (error) {
+    queue.push(error.toString());
+  } finally {
     queue.push('Guardrail was processed');
   }
   return queue;
