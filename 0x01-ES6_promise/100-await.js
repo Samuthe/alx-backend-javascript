66bab22aba493d54a87135887c5ca9fc908a889a
Samuthe <ekatisamuel@gmail.com>
2023-01-31 14:34:53 +0300

    100-await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line import/no-unresolved
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const resFromUploadPhoto = await uploadPhoto();
+    const resFromCreateUser = await createUser();
+    return {
+      photo: resFromUploadPhoto,
+      user: resFromCreateUser,
+    };
+  } catch
+  (err) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
