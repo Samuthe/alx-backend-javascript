9a93af778a0ae4c0fdecd3b29242852ae0e2b5da
Samuthe <ekatisamuel@gmail.com>
2023-02-01 09:53:26 +0300

    0-listStudents

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,7 @@
-function getListStudents() {
+export default function getListStudents() {
   return [
     { id: 1, firtsName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firtsName: 'James', location: 'Columbia' },
     { id: 5, firtsName: 'Serena', location: 'San Francisco' },
   ];
 }
-getListStudents();
