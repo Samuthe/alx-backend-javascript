2f32b3285d5b48253efed7b6bcfe1c4d46b37aec
Samuthe <ekatisamuel@gmail.com>
2023-02-02 09:46:42 +0300

    sum up

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,9 @@
 export default function getStudentIdsSum(students) {
   if (students instanceof Array) {
-    return students.reduce((prevStudent, curStudent) => prevStudent || prevStudent + curStudent, 0);
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
   }
   return 0;
 }
