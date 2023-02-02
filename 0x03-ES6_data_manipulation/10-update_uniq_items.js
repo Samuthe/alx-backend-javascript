32229720690a9a358655b7853f34ba4c1edf433e
Samuthe <ekatisamuel@gmail.com>
2023-02-02 18:42:47 +0300

    update unique items

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,7 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
+  map.forEach((x) => {
+    if (x === 1) map.set(x, 100);
+  });
+  return map;
+}
