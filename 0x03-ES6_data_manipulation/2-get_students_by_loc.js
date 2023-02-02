f1abf2f1ab7ed9a4e139e8ef3a52a4c22ef7236a
Samuthe <ekatisamuel@gmail.com>
2023-02-02 09:19:34 +0300

    listStudents

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,5 @@
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  } return [];
+}
