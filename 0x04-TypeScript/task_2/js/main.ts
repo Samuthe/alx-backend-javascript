0ae4019111497365776948d904d2b4a25d160280
Samuthe <ekatisamuel@gmail.com>
2023-02-16 09:43:09 +0300

    typescript task_2

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -51,3 +51,12 @@ export function executeWork(employee: (Teacher | Director)){
     }
     return (employee as Teacher).workTeacherTasks()
 }
+
+export type Subjects = 'Math' | 'History';
+
+export function teachClass(todayClass: Subjects): string {
+    if (todayClass == 'Math'){
+        return 'Teaching Math'
+    }
+    return 'Teaching History'
+}
