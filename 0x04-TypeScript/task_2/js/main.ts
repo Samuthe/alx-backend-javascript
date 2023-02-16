84e929ec02d0932845103be64217e0d5ac55ac90
Samuthe <ekatisamuel@gmail.com>
2023-02-16 09:20:58 +0300

    typescript task_2

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,42 @@
+export interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+export interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+export class Director implements DirectorInterface {
+    workFromHome(){
+        return 'Working from home'
+    }
+    getCoffeeBreak() {
+        return 'Getting a coffee break'        
+    }
+    workDirectorTasks() {
+        return 'Getting to director tasks'
+    }
+}
+
+export class Teacher implements TeacherInterface {
+    workFromHome() {
+        return 'Cannot work from home'        
+    }
+    getCoffeeBreak() {
+        return 'Cannot have a break'        
+    }
+    workDirectorTasks() {
+        return 'Getting to work'
+    }
+}
+
+export function createEmployee(salary: (number | string)):(Director | Teacher) {
+    if(typeof salary === 'number' && salary < 500){
+        return new Teacher();
+    }
+    new Director();
+}
