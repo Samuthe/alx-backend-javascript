ce254faf7e895dd9a4353025952040ba0859921d
Samuthe <ekatisamuel@gmail.com>
2023-03-21 18:17:20 +0300

    1-stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,17 +5,30 @@
  *  - Notifies user when exiting
  */
 
-const { stdout, stdin } = process;
+// const { stdout, stdin } = process;
 
-stdout.write('Welcome to Holberton School, what is your name?\n');
+// stdout.write('Welcome to Holberton School, what is your name?\n');
 
-stdin.setEncoding('utf-8');
-stdin.on('readable', () => {
-  const name = stdin.read();
-  stdout.write(`Your name is: ${name}`);
-  process.exit();
+// stdin.setEncoding('utf-8');
+// stdin.on('readable', () => {
+//   const name = stdin.read();
+//   stdout.write(`Your name is: ${name}`);
+//   process.exit();
+// });
+
+// process.on('exit', () => {
+//   stdout.write('This important software is now closing\n');
+// });
+
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-process.on('exit', () => {
-  stdout.write('This important software is now closing\n');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
