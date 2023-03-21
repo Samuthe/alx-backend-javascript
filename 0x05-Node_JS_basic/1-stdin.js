fad5b38dfbbb4a24cf06f25c1939b8ddf0836605
Samuthe <ekatisamuel@gmail.com>
2023-03-21 18:11:33 +0300

    1-stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,12 @@
-const { stdout, stdin } = process;
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-stdout.write('Welcome to Holberton School, what is your name?\n');
-
-stdin.setEncoding('utf-8');
-stdin.on('readable', () => {
-  const name = stdin.read();
-  stdout.write(`Your name is: ${name}`);
-  process.exit();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-process.on('exit', () => {
-  stdout.write('This important software is now closing\n');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
