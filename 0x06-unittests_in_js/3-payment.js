35c53d1a8d3d2b13ca55d341449cb24c344a193e
Samuthe <ekatisamuel@gmail.com>
2023-04-03 08:43:06 +0300

    payment

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,9 +1,8 @@
-Utils = require('./utils');
+const Utils = require('./utils');
 
-Utils = require('./utils');
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-    const output = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-    console.log(`The total is: ${output}`);
+	const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+	console.log(`The total is: ${result}`);
 }
 
 module.exports = sendPaymentRequestToApi;
