068371533810c90b6b701f9a2151499df25f18b5
Samuthe <ekatisamuel@gmail.com>
2023-04-03 11:04:21 +0300

    10-api_test

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -4,28 +4,27 @@ const app = express();
 const port = 7865;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the payment system')
+  res.send('Welcome to the payment system');
 });
 
 app.get('/cart/:id([0-9]+)', (req, res) => {
-    console.log(`${req.params.id}`);
-    res.end(`Payment methods for cart ${req.params.id}`);
+  res.end(`Payment methods for cart ${req.params.id}`);
 });
 
 app.get('/available_payments', (req, res) => {
-    res.json({
-        payment_methods:{
-            credit_cards: true,
-            paypal: false,
-        }
-    });
+  res.json({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    }
+  });
 });
 
-app.post('login', (req, res) => {
-    const {userName} = req.body;
-    res.end(`Welcome ${userName}`)
+app.post('/login', (req, res) => {
+  const { userName } = req.body;
+  res.end(`Welcome ${userName}`);
 });
 
 app.listen(port, () => {
-    console.log(`API available on localhost port ${port}`);
+  console.log(`API available on localhost port ${port}`);
 });
